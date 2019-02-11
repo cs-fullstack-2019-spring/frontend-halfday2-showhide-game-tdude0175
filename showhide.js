862e2dc2277d5923e59cfbe41b5f58d74783a4e6
Kevin-CodeCrew <kevin@code-crew.org>
2019-02-11 07:24:04 -0600

    minor updates

--- showhide.js
+++ showhide.js
@@ -2,6 +2,7 @@ let clicks = 0;
 let targets = 0;
 let hits = 0;
 
+// Point of Entry called from HTML when page is loaded
 function letsRock() {
     let theGo = document.getElementById("goGetIt");
     theGo.onclick = function () {
@@ -9,7 +10,7 @@ function letsRock() {
         const targetKount = document.getElementById("numberOfTargets").value;
         const targetTime = document.getElementById("displayTime").value;
         // No start the game!
-        setUpTargetsAndPlay(targetKount, targetTime);
+        setUpTargetsAndPlay(parseInt(targetKount), parseInt(targetTime));
     };
 }
 // Utility function to get a random table cell number
@@ -19,14 +20,18 @@ function getRandomInt(min, max) {
 // This function gets called if player hits a target
 function clickedTarget(e) {
     console.log(e.target);
-    console.log(e.target.querySelector("img"));
-    if (e.target.querySelector("img").style.display != null) {
-        e.target.querySelector("img").style.display = 'block';
+    // Let's get the hit item and store in a variable
+    let hit = e.target.querySelector("img");
+
+    // FIXME: Targets clicked before they disappear should stay visible. Presently they do not.
+    /*  Do some sanity checks making sure there is an image and it has the 'display' style
+        before we try to change the display property.
+    */
+    if (hit != null && hit.style.display != null) {
+        e.target.querySelector("img").style.display = 'block'; // Make hit target image visible again
     }
     console.log("Got a Hit!");
-    // Right now, just updates a count.
-    // Could use some player feedback here
-
+    // Update their hit score
     hits += 1;
 }
 // The main function that sets up targets and starts a game
@@ -40,7 +45,8 @@ function setUpTargetsAndPlay(numberOfTargets, displayTimeMs) {
 
         clicks += 1;
         console.log("clicked = " + clicks + " Max = " + targets);
-        if (clicks == targets) {
+        if (clicks === targets) {
+            // FIXME: Sometime at end of game hits are more than 5 for some reason which should be impossible
             alert("No more clicks! You got " + hits + " out of " + targets);
             // Turn off click detection
             $("td").off("click");
@@ -49,7 +55,7 @@ function setUpTargetsAndPlay(numberOfTargets, displayTimeMs) {
         }
     });
 
-    console.log("Selecting " + targets + " targets")
+    console.log("Selecting " + targets + " targets");
     // Get the number of targets specified and randomly picks cells to display them in for the target table
     for (let x = 0; x < targets; x++) {
         let targetNum = getRandomInt(1, 50); // Pick a random table cell
@@ -57,9 +63,10 @@ function setUpTargetsAndPlay(numberOfTargets, displayTimeMs) {
         let tdID = "td" + targetNum;
         let imgID = "img" + targetNum;
 
+        // Set an IMG for each randomly selected cell along with 'click' event handler
         $('#' + tdID).on("click", clickedTarget).append("<img id = " + imgID + " class= 'targetImg' src='bird.png'>");
-        $(`#${imgID}`).delay(2000).show(0);
-        $(`#${imgID}`).delay(displayTimeMs).hide(0);
+        $('#' + imgID).delay(2000).show(0); // Wait 2 seconds then show the targets
+        $('#' + imgID).delay(displayTimeMs).hide(0); // Setup a callback that will hide the images after the specified time
     }
 
-};
+}
